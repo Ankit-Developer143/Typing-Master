f27186c982cdafa6b5a0d3aa145f210f4f8e4440
dante7785@gmail.com <dante7785@gmail.com>
2021-03-28 21:15:53 +0530

    Typing Master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModules} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModules
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card'
+
+
+
+
+@NgModule({
+  exports: [
+    MatInputModule,
+    MatCardModule
+    
+  ]
+})
+export class MaterialModule { }
